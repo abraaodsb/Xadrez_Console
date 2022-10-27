08c4e38f94ab0bb57485294a334db2323c161101
Abraão dos santos batista <abraas898@gmail.com>
2022-10-27 20:40:50 -0300

    Seção 12: Criando a Classe Posição Xadrez

---
+++ Xadrez_Console/Xadrez/Rei.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabuleiro;
+
+namespace Tabuleiro 
+{
+    class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        { }
+        public override string ToString()
+        {
+            return "R";
+            
+        }
+    
+    }
+}
