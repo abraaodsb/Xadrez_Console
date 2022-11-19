820903299f330ff268b00a47dcc5aebd6bf76789
Abraão dos santos batista <abraas898@gmail.com>
2022-11-19 15:01:49 -0300

    Secao 12: Fim do Projeto do Jogo de Xadrez, jogada especial Promoção

--- Xadrez_Console/Xadrez/PartidaDeXadrez.cs
+++ Xadrez_Console/Xadrez/PartidaDeXadrez.cs
@@ -121,6 +121,23 @@ namespace Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -139,8 +156,6 @@ namespace Xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //#jogadaespecial en Passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
